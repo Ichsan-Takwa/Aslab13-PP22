69d31bbe6375a0e0b13f986e679ed68d5e24d4c4
MuchtarAdam <93690489+MuchtarAdam@users.noreply.github.com>
2022-09-23 10:21:40 +0800

    Add files via upload
    Belum Selesai No.3

---
+++ Tugas 2 Pengantar Pemrograman/No.3.py
@@ -0,0 +1,19 @@
+a,b,c,d,e = [int(x) for x in input(). split]
+
+
+if a%2 = 0:
+    print("Angka Genap")
+else a%2 !=0:
+    print("Angka Ganjil")
+
+if b%2 = 0:
+    print("Angka Genap")
+
+if c%2 = 0:
+    print("Angka Genap")
+
+if d%2 = 0:
+    print("Angka Genap")
+
+if e%2 = 0:
+    print("Angka Genap")
