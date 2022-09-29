cf1dac477901932e129be6593c2b9e880b05b676
Zabryna Andiny <113653837+ZabrynaAndiny11@users.noreply.github.com>
2022-09-29 22:15:47 +0800

    Tugas no.2 diperbaharui

--- Pekan 2/no. 2.py
+++ Pekan 2/no. 2.py
@@ -7,22 +7,28 @@ pemakaian = int(input("pemakaian listrik : "))
 if golongan == 'R1' :
     if daya == 900 :
         print(f"jumlah tagihan : {1352*pemakaian}")
-    elif daya >= 1350 and daya <= 2200 :
+    elif daya == 1350 :
+        print(f"jumlah tagihan : {1444.70*pemakaian}")
+    elif daya == 2200 :
         print(f"jumlah tagihan : {1444.70*pemakaian}")
     else :
         print("Data tidak valid")
 elif golongan == 'R2' :
-    if daya >= 3500 and daya <= 5500 :
+    if daya == 3500 :
+        print(f"jumlah tagihan : {1699.53*pemakaian}")
+    elif daya == 5500 :
         print(f"jumlah tagihan : {1699.53*pemakaian}")
     else : 
         print("Data tidak valid")
 elif golongan == 'R3' :
-    if daya >= 6600 : 
+    if daya == 6600 : 
         print(f"jumlah tagihan : {1699.53*pemakaian}")
     else : 
         print("Data tidak valid")
 elif golongan == 'B2' :
-    if daya >= 6600 and daya <= 200000 :
+    if daya >= 6600 :
+        print(f"jumlah tagihan : {1444.70*pemakaian}")
+    elif daya >= 200000 :
         print(f"jumlah tagihan : {1444.70*pemakaian}")
     else : 
         print("Data tidak valid")
