38db722ba6ab05c881307a99df30da2203559109
HenokhIS <113653732+HenokhIS@users.noreply.github.com>
2022-10-02 22:25:30 +0800

    Update praktikum3_pp_H071221045_no_2.py

--- Pekan-3/praktikum3_pp_H071221045_no_2.py
+++ Pekan-3/praktikum3_pp_H071221045_no_2.py
@@ -1,5 +1,3 @@
-import math
-
 derajat = float(input("Masukkan derajat matahari (0 - 360 derajat): "))
 
 hitung = float(derajat/15)
@@ -12,22 +10,14 @@ detik = int((jumlah % 3600) % 60)
 
 if jam > 24:
     jam1 = jam - 24
-    if jam1 >= 6 and jam1 < 12:
-        print("Selamat Pagi")
-    elif jam1 >= 12 and jam1 < 16:
-        print("Selamat Siang")
-    elif jam1 >= 16 and jam1 < 19:
-        print("Selamat Sore")
-    else:
-        print("Selamat Malam")
-    print(f"{jam1:02d}:{menit:02d}:{detik:02d}")
+
+if jam >= 6 and jam < 12:
+    print("Selamat Pagi")
+elif jam >= 12 and jam < 16:
+    print("Selamat Siang")
+elif jam >= 16 and jam < 19:
+    print("Selamat Sore")
 else:
-    if jam >= 6 and jam < 12:
-        print("Selamat Pagi")
-    elif jam >= 12 and jam < 16:
-        print("Selamat Siang")
-    elif jam >= 16 and jam < 19:
-        print("Selamat Sore")
-    else:
-        print("Selamat Malam")
-    print(f"{jam:02d}:{menit:02d}:{detik:02d}")
+    print("Selamat Malam")
+
+print(f"{jam:02d}:{menit:02d}:{detik:02d}")
