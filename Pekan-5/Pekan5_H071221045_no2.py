dbc8abe69e306c4d7b6df78e50cdaad4a8bb996f
Ichsan-Takwa <72344488+Ichsan-Takwa@users.noreply.github.com>
2022-11-12 10:24:49 +0800

    Delete Pekan-5 directory

--- Pekan-5/Pekan5_H071221045_no2.py
+++
@@ -1,9 +0,0 @@
-list1 = list(map(int, input("Input array pertama: ").split()))
-list2 = list(map(int, input("Input array kedua: ").split()))
-
-print(f"\nArray Pertama: {list1}")
-print(f"Array Kedua: {list2}")
-
-sama = set(list1).intersection(list2)
-
-print(f"\nTerdapat {len(sama)} duplikasi {sama}")
