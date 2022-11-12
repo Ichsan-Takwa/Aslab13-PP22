d4b025292d9d52c04aba0dafe0046b34101c0679
Ichsan-Takwa <72344488+Ichsan-Takwa@users.noreply.github.com>
2022-11-12 10:23:53 +0800

    Delete Pekan-1 directory

--- Pekan-1/praktikum1_pp_H071221045_no_3.py
+++
@@ -1,7 +0,0 @@
-rata = int(input("Masukkan rata-rata pemakaian listrik harian (Wh): "))
-
-kwh = rata / 1000
-tarif = (kwh * 1325) * 30
-round(tarif, 2)
-
-print("Jumlah tagihan listrik bulanan: Rp.", tarif)
